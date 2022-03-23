2b32a7c8e6f24dee1d14e3cda2394da3b5e6cf21
RITEKROUNAK <ritekrounak1@gmail.com>
2022-03-23 09:31:42 +0530

    :alien: Error Page

---
+++ resources/views/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title', 'Error')
+
+@section('content')
+    <div class="content-minify">
+        <div class="container">
+            <div class="error-page">
+                <div class="text-center">
+                    <h1>Error 404!</h1>
+                    <h2>Page not found</h2>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@stop
