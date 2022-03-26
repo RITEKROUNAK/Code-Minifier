0f1c33193466d3a2d1328f3f72fefb758fba6263
RITEKROUNAK <ritekrounak1@gmail.com>
2022-03-26 23:32:47 +0530

    :page_facing_up: Mail Templated Added

---
+++ resources/views/mails/contact.blade.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+</head>
+<body style="margin: 0px;">
+
+<div style="float: left; width: 100%; background: #FFF;">
+
+    <div style="float: left; background-color: #00BC8C; margin-bottom: 15px; text-align: center; width: 100%;">
+        <h4 style="font-size: 17px; padding: 15px 5px; text-align: center; margin: 10px; color:#FFF;">Code Minify</h4>
+    </div>
+
+    <div style="float: left; color: #333; width: 100%">
+        <p style="font-size: 14px; text-align: left; margin: 10px 0;">Contact page Submitted by {{$request->name}}</p>
+    </div>
+    <p>Email: {{$request->email}}</p>
+    <p>Message: {{$request->message}}</p>
+
+    <div style="width:100%; float:left; padding: 8px 0; margin-top: 50px;">
+        <p>© Code Minifier by <a href="https://ritekrounak.github.io/portfolio/" target="_blank"><span>@</span>Ritek</a></p>
+    </div>
+</div>
+
+</body>
+</html>
